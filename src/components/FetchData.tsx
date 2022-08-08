4a7c22b808ab3743798e0d50cb22483cec6c460a
Somin <sominupadhyaya@gmail.com>
2022-08-08 18:07:24 +0545

    complete fetching instance

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+// import { useState } from "react"
+import api from "../api/api"
 
 const  FetchData = () => {
+  // const [fetchData, setData] = useState([])
+
+  const fetchData = async () =>{
+    const response = await api.get('/posts')
+    return response.data
+  }
+  
+  const {data} = useQuery(["posts"], fetchData)
+  
+  console.log(data)
   return (
     <div>FetchData</div>
   )
