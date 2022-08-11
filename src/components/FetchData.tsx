98d1715ab85b9afa25ee32c0af6850a46eb75458
Somin <sominupadhyaya@gmail.com>
2022-08-11 10:40:48 +0545

    make changes in displaying

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -8,43 +8,51 @@ const  FetchData:FC = () => {
   
   const [requestType, setReqType] = useState<string>(POSTS)
 
-  const fetchData = async ({ queryKey }) =>{
+  const fetchData = async ({ queryKey } : null | any) =>{
     const response = await api(queryKey[1])
     
     return response.data
   }
 
-  const {data, status , isPreviousData} = useQuery(["posts" , requestType], fetchData , {
+  const {data, isLoading , isPreviousData} = useQuery(["posts" , requestType], fetchData , {
     keepPreviousData : true
   })
   
   
+console.log(data)
 
-
-  if(status === 'loading') {
+  if(isLoading) {
     return (
       <h1>Loading</h1>
     )
   } 
 
-  
+  if(requestType === POSTS){
+  data.map((post: any | null) =>{
+    return (
+      <div key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  )  
+})
+}
 
-
-  
+if(requestType === COMMENTS){
+  data.map((comment : any | null) =>{
+    return(
+      <div key={comment.id}>
+        <div>{comment.name}</div>
+        <p>{comment.body}</p>
+      </div>
+    )
+  })
+} 
   return (
     <>
-    <button onClick={() => setReqType(COMMENTS)}>Watch comments</button>
-    {
-      data.map((post: any | null) =>{
-        return (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        )  
-      })
-    }
+    <button onClick={() => setReqType(COMMENTS)}>Watch Comments</button>
+    <button onClick={() => setReqType(POSTS)}>Watch Posts</button>
     </>
-  )
-}   
+)
+  }
 export default FetchData
